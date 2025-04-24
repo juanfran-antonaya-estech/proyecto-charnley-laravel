e6296da3efa427a5a27643dbf11b4b51ba241ee7
Juan Francisco Antonaya Lopez <juanfran.antonaya@escuelaestech.es>
2025-04-24 19:43:08 +0200

    Añadidos endpoints de /user

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,17 @@
 <?php
 
+use App\Http\Controllers\V0_5\UserController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
+use App\Http\Middleware\Authenticate;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V0_5\AuthController;
 use Laravel\Jetstream\Http\Middleware\AuthenticateSession;
-use App\Http\Middleware\Authenticate;
 
 //Rutas sin autorizacion
 
 Route::prefix('/V0_5')->group(function () {
-    Route::middleware([Authenticate::class])->group(function () {
+    Route::middleware([AuthenticateSession::class])->group(function () {
         Route::prefix('/auth')->group(function () {
             Route::post('/login', [AuthController::class, 'login']);
 
@@ -22,7 +23,11 @@ Route::prefix('/V0_5')->group(function () {
         });
 
         Route::prefix('/user')->group(function() {
-
+            Route::post('/uploadImage', [UserController::class, 'uploadImage']);
+            Route::get('/photo', [UserController::class, 'listAllOwnPhotos']);
+            Route::get('/photo/{id}', [UserController::class, 'getPhotoDetail']);
+            Route::get('/chat', [UserController::class, 'getChatrooms']);
+            Route::get('/chat/{id}', [UserController::class, 'getMessagesByChatroom']);
         });
 
         Route::prefix('/familiar')->group(function() {
