09790743f0371a835584ba234c28b854f0270a5e
Jose Lopez Vilchez <jose.lopez@escuelaestech.es>
2025-04-29 22:14:30 +0200

    retoque

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -106,19 +106,9 @@
 
                         <x-slot name="content">
                             <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-
                             <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
+                                {{ __('perfil') }}
                             </x-dropdown-link>
-
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                    {{ __('API Tokens') }}
-                                </x-dropdown-link>
-                            @endif
 
                             <div class="border-t border-gray-200"></div>
 
