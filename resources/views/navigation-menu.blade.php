f1a1d2ffb4e9fad8b41f1ee4b8eb9a280d3da996
DPTO Investigacion y Desarrollo LINARED INFORMATICA <dpto.investigacionydesarrollolinaredinformatica@MacBook-Pro-de-Dpto.local>
2025-05-14 17:18:52 +0200

    el log out funciona (al menos a mi) y no hace falta hacer click en las letras, funciona en toda la cajita

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -24,26 +24,20 @@
             <div class="dropdown dropdown-end">
                 <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
                     <div class="w-10 rounded-full">
-                        <img
-                        alt="Tailwind CSS Navbar component"
-                        {{--
-                        alt="{{ Auth::user()->name }}"
-                        src="{{ Auth::user()->profile_photo_url }}" />
-                        --}}
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" /> <!-- placeholder hasta tener imagenes -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9" />
+                        </svg>
                     </div>
                 </div>
-                <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li><a href="{{ route('profile.show') }}">{{ __('perfil') }}</a></li>
-                    <li>
-                        <form method="POST" action="{{ route('logout') }}" x-data>
-                            @csrf
-                            <a href="{{ route('logout') }}" @click.prevent="$root.submit();">
-                                {{ __('Log Out') }}
-                            </a>
-                        </form>
-                    </li>
-                </ul>
+                <form method="POST" action="{{ route('logout') }}" x-data>
+                    @csrf
+                    <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li>
+                            <a href="{{ route('logout') }}" @click.prevent="$root.submit();">{{ __('Log Out') }}</a>
+                        </li>
+                    </ul>
+                </form>
+                
             </div>
         </div>
     </div>
