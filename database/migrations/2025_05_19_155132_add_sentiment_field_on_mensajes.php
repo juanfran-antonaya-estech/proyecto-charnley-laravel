c55f76000365572f8b191a63b969ab8a2f44c212
Juan Francisco <juanfran.antonaya@escuelaestech.es>
2025-05-19 17:52:57 +0200

    Añadidas cosas de reportes y cositas de mensajes irrelevantes

---
+++ database/migrations/2025_05_19_155132_add_sentiment_field_on_mensajes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mensajes', function (Blueprint $table) {
+            $table->string('sentiment')->nullable()->after('mensaje');
+            $table->double('sentiment_score')->nullable()->after('sentiment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mensajes', function (Blueprint $table) {
+            $table->dropColumn('sentiment');
+            $table->dropColumn('sentiment_score');
+        });
+    }
+};
