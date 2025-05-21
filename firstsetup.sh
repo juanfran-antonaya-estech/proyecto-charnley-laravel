7d1ef84095af4e2729fd985ef5bda459a51a7680
Juan Francisco <juanfran.antonaya@escuelaestech.es>
2025-05-21 16:17:15 +0200

    Remove unnecessary variable unset command from first setup script

--- firstsetup.sh
+++ firstsetup.sh
@@ -26,3 +26,4 @@ npm update
 current_dir=$(pwd)
 osascript -e "tell application \"Terminal\" to do script \"cd '$current_dir' && php artisan serve\""
 osascript -e "tell application \"Terminal\" to do script \"cd '$current_dir' && npm run dev\""
+unset current_dir
